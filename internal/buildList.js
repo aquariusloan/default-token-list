bc48ff72e631c3197a40b9d9d7a27bb78eb74f4b
developer <davittchautchidze@gmail.com>
2022-01-25 21:32:28 +0100

    created project

---
+++ internal/buildList.js
@@ -0,0 +1,34 @@
+const { version } = require("../package.json");
+
+// const mainnet = require("../assets/tokens/mainnet.json");
+const cronos = require("../assets/tokens/cronos.json");
+const cronosTestnet = require("../assets/tokens/cronos-testnet.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "AgileFinance Menu",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI:
+      "https://raw.githubusercontent.com/agiledefi/default-token-list/master/assets/logox200.png",
+    keywords: ["AgileFinance", "default"],
+    tokens: [
+      // ...mainnet,
+      ...cronos,
+      ...cronosTestnet,
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
